dc078730946ccce4044d3cd949a52e4c94755f3f
ehhglen <47801406+ehhglen@users.noreply.github.com>
2019-10-10 15:08:54 -0600

    List Class

---
+++ List.java
@@ -0,0 +1,31 @@
+public abstract class List implements MyCollectionInterface {
+
+    // Declare variables
+
+    Node head;
+    int numberOfEntries;
+
+
+   public boolean add(T newItem) {
+       Node newNode;
+       newNode.setData = newItem;
+
+       try {
+           if(numberOfEntries == 0) {
+               firstNode = newNode;
+           } // End if
+           else {
+               newNode.setNextNode = firstNode;
+               firstNode = newNode;
+           }
+       } // End try
+
+       catch(Exception e) {
+           System.err.println("Unable to add item to the list.");
+       }
+
+
+       
+   } // End add
+
+} // End class 
