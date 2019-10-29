16ff736d641787cced517fe678c63197536059fd
ehhglen <47801406+ehhglen@users.noreply.github.com>
2019-10-29 10:54:56 -0600

    changed the add methods

--- List.java
+++ List.java
@@ -5,26 +5,62 @@ public abstract class List implements MyCollectionInterface<T> {
     private Node head;
     private Node firstNode;
     private int numberOfEntries;
+    private boolean result;
 
-    @Override
+//*****************************************************************
+
+/**
+ * Adds a new entry to the LinkedList
+ * @param newItem
+ * @return True if successful; false if not
+ */
+
    public boolean add(T newItem) {
-       if ((givenPosition >= 1) && (givenPosition <= numberOfEntries + 1)) {
-           Node newNode = new Node(newItem);
-           if(givenPosition == 1) { // Adding to beginning of chain
-               newNode.setNextNode(firstNode);
-               firstNode = newNode;
-           } // End inner if
-           else {                   // Adding to position not beginning of chain
-               Node nodeBefore = getNodeAt(givenPosition - 1);
-               Node nodeAfter= nodeBefore.getNextNode();
-               newNode.setNextNode(nodeAfter);
-               nodeBefoere.setNextNode(newNode);
-           } // End else
-       } // End if
-       numberOfEntries++;
-      
-      
+
+        Node newNode = new Node(newItem);
+
+        newNode.setNextNode = head;
+        head = newNode;
+
+        if(head.equals(newNode)) {
+            numberOfEntries++;
+        }
+        return result; 
+       
    } // End add
+
+//*****************************************************************
+
+public boolean add(int givenPosition, T newItem) {
+    result = false;
+
+    if((givenPosition >= 0) && (givenPosition <= numberOfEntries)) {
+
+        Node<T> newNode = newNode<T> (newItem);
+
+        if(givenPosition == 0) 
+        {
+            newNode.setNextNode = head;
+            head = newNode;
+            numberOfEntries++;
+            add(newItem);
+            result = true;
+        } else {
+            Node<T> currentNode = head;
+            for(int i = 1; i < givenPosition; i++) 
+            {
+                currentNode = currentNode.next;
+            } // End for loop
+
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+            numberOfEntries++;
+        } // End else
+    } else {
+        throw new IndexOutOfBoundsException("The given position cannot be found.");
+    } // End else
+    return result;
+} // End add method
 
    @Override
    public boolean remove() { // Why is the return type incompatible?
